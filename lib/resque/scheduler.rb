4f5c2a2ef24f2fc1ab096e839980237e56c85da0
Aaron Suggs <aaron@ktheory.com>
2013-09-18 13:21:15 -0400

    Shutdown gracefully in ruby 2.0.0.
    Fixes issue #228.
    
    Avoid `ThreadError: can't be called from trap context` on shutdown.
    
    Using mutexes inside traps is deadlockable and prohibited in ruby 2.0.
    Instead, do the work in a seperate thread.
    
    See discussion at https://bugs.ruby-lang.org/issues/7917

--- lib/resque/scheduler.rb
+++ lib/resque/scheduler.rb
@@ -297,7 +297,7 @@ module Resque
       def shutdown
         @shutdown = true
         if @sleeping
-          release_master_lock!
+          Thread.new { release_master_lock! }
           exit
         end
       end
